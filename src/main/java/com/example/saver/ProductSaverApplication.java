3e42383527ec81919952d886cad6afe1fea2ea27
Anatolii Omelchenko <omelchenko20@gmail.com>
2023-08-04 10:33:05 +0300

    initial commit

---
+++ src/main/java/com/example/saver/ProductSaverApplication.java
@@ -0,0 +1,13 @@
+package com.example.saver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductSaverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductSaverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/saver/ProductSaverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.saver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductSaverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
